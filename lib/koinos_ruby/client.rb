0eee3b7bbe50c000db25c47c78930511dfa780ba
Yoni Amir <yoni.amir3@gmail.com>
2024-07-26 19:33:51 -0400

    Initial commit

--- lib/koinos_ruby/client.rb
+++ lib/koinos_ruby/client.rb
@@ -1,4 +1,3 @@
-require 'pry'
 require "faraday"
 require "json"
 
@@ -6,155 +5,101 @@ module KoinosRuby
   class Client
     API_BASE_URL = "https://api.koinos.io"
 
-    def initialize(api_key, api_base_url = API_BASE_URL)
-      @api_key = api_key
+    def initialize(api_base_url = API_BASE_URL)
       @connection = Faraday.new(url: api_base_url) do |conn|
         conn.request :json
         conn.response :json, content_type: /\bjson$/
         conn.adapter Faraday.default_adapter
-        conn.headers['X-API-KEY'] = api_key
       end
     end
 
-    # Block Endpoints
-    def get_block_by_id(block_id)
-      get("blocks/#{block_id}")
+    # Accounts Endpoints
+    def account_balance(account_id)
+      get("/accounts/#{account_id}/balance")
     end
 
-    def get_blocks_by_height(start_height, end_height)
-      get("blocks", { start_height: start_height, end_height: end_height })
-    end
-
-    def get_blocks_by_producer(producer_id)
-      get("blocks", { producer: producer_id })
-    end
-
-    # Transaction Endpoints
-    def get_transaction_by_id(transaction_id)
-      get("transactions/#{transaction_id}")
-    end
-
-    def get_transactions_by_block(block_id)
-      get("blocks/#{block_id}/transactions")
-    end
-
-    def get_transactions_by_account(account_id)
-      get("transactions", { account: account_id })
-    end
-
-    # Account Endpoints
-    def get_account_balance(account_id)
-      get("accounts/#{account_id}/balance")
-    end
-
-    def get_account_nonce(account_id)
-      get("accounts/#{account_id}/nonce")
-    end
-
-    def get_account_resources(account_id)
-      get("accounts/#{account_id}/resources")
-    end
-
-    def get_account_history(account_id, limit = 10, start = nil)
+    def account_history(account_id, limit = 10, start = nil)
       params = { limit: limit }
       params[:start] = start if start
-      get("accounts/#{account_id}/history", params)
+      get("/accounts/#{account_id}/history", params)
     end
 
-    # Event Endpoints
-    def get_event_by_id(event_id)
-      get("events/#{event_id}")
+    def account_mana(account_id)
+      get("/accounts/#{account_id}/mana")
     end
 
-    def get_events_by_block(block_id)
-      get("blocks/#{block_id}/events")
+    def account_next_nonce(account_id)
+      get("/accounts/#{account_id}/next_nonce")
     end
 
-    def get_events_by_transaction(transaction_id)
-      get("transactions/#{transaction_id}/events")
+    def account_nonce(account_id)
+      get("/accounts/#{account_id}/nonce")
     end
 
-    def get_events_by_account(account_id)
-      get("accounts/#{account_id}/events")
+
+    # Blocks Endpoints
+    def block_by_id(block_id)
+      get("/blocks/#{block_id}")
     end
 
-    # Token Endpoints
-    def get_token_info(token_id)
-      get("tokens/#{token_id}")
+
+    # Chain Endpoints
+    def fork_heads
+      get("/chain/fork_heads")
     end
 
-    def get_token_balance(account_id, token_id)
-      get("tokens/#{token_id}/balances/#{account_id}")
+    def head_info
+      get("/chain/head_info")
     end
 
-    def get_token_transfers(account_id, token_id, limit = 10, start = nil)
-      params = { limit: limit }
-      params[:start] = start if start
-      get("tokens/#{token_id}/transfers/#{account_id}", params)
+    def chain_id
+      get("/chain/id")
     end
 
-    # Governance Endpoints
-    def get_governance_parameters
-      get("governance/parameters")
+    def chain_resource_limits
+      get("/chain/resource_limits")
     end
 
-    def get_governance_proposals(status = nil)
-      params = {}
-      params[:status] = status if status
-      get("governance/proposals", params)
-    end
-
-    def get_governance_votes(proposal_id)
-      get("governance/proposals/#{proposal_id}/votes")
-    end
-
-    # Utility Endpoints
-    def get_status
-      get("status")
-    end
-
-    def get_chain_id
-      get("chain_id")
-    end
-
-    # Mana Endpoints
-    def get_account_mana(account_id)
-      get("accounts/#{account_id}/mana")
-    end
-
-    def get_system_mana
-      get("system/mana")
-    end
 
     # Contract Endpoints
-    def get_contract_metadata(contract_id)
-      get("contracts/#{contract_id}/metadata")
+    def contract_read(contract_id, method)
+      get("/contract/#{contract_id}/#{method}")
     end
 
-    def get_contract_state(contract_id, key)
-      get("contracts/#{contract_id}/state/#{key}")
+    def contract_read_with_args(contract_id, method, args)
+      post("/contract/#{contract_id}/#{method}", args)
     end
 
-    # POST Endpoints
-    def submit_transaction(transaction)
-      post("transactions", transaction)
+    def contract_abi(contract_id)
+      get("/contract/#{contract_id}/abi")
     end
 
-    def submit_contract_call(contract_call)
-      post("contracts/call", contract_call)
+
+
+    # Decode Endpoints
+
+    # Fungible Tokens Endpoints
+
+    # Non Fungible Tokens Endpoints
+
+    # Transactions Endpoints
+    def transaction_by_id(transaction_id)
+      get("/transaction/#{transaction_id}")
     end
 
-    def submit_proposal(proposal)
-      post("governance/proposals", proposal)
+    def transaction_prepare
+      post("/transactions/prepare", {})
     end
 
-    def submit_vote(vote)
-      post("governance/votes", vote)
+    def transaction_submit(transaction)
+      post("/transactions", transaction)
     end
 
     private
 
     def get(path, params = {})
+      full_url = URI.join(API_BASE_URL, path).to_s
+      puts "Full URL: #{full_url}"
       response = @connection.get(path, params)
       handle_response(response)
     end
